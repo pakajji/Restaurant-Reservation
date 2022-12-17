535f653b94d97f4c7e041885badfd0bafcbc7de0
pakajji <114389230+pakajji@users.noreply.github.com>
2022-12-17 12:30:20 +0700

    ...

--- resources/views/admin/menus/create.blade.php
+++ resources/views/admin/menus/create.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex m-2 p-2">
-                <a href="{{route('admin.categories.index')}}" class="px-4 py-2 bg-yellow-400 hover:bg-yellow-500 rounded-lg text-black">Category Index</a>
+                <a href="{{route('admin.menus.index')}}" class="px-4 py-2 bg-yellow-400 hover:bg-yellow-500 rounded-lg text-black">Menu Index</a>
             </div>
             <div class="m-2 p-2">
                 <form>
--- resources/views/admin/reservation/index.blade.php
+++ resources/views/admin/reservation/index.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-end m-2 p-2">
-                <a href="{{route('admin.categories.create')}}" class="px-4 py-2 bg-yellow-400 hover:bg-yellow-500 rounded-lg text-black">Create Category</a>
+                <a href="{{route('admin.reservation.create')}}" class="px-4 py-2 bg-yellow-400 hover:bg-yellow-500 rounded-lg text-black">Create Reservation</a>
             </div>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
